f3fbbce50bade5c9bfa28b286feabc5756b0b4f3
Marcus loo <Marcuslooek@gmail.com>
2025-06-16 14:18:06 +0800

    no message

--- app/Livewire/Projects/EditProject.php
+++ app/Livewire/Projects/EditProject.php
@@ -40,8 +40,13 @@ class EditProject extends Component
 
     public function save(): void
     {
+        if (Auth()->user()->hasRole(User::ROLE_STUDENT) && $this->project->created_by !== auth()->id()) {
+            $this->error("Cannot update project that does not belong to you", redirectTo: route('projects.edit', $this->project->getRouteKey()));
+            return;
+        }
+
         if (auth()->user()->hasRole(User::ROLE_STUDENT) && $this->project->status == Project::STATUS_APPROVED) {
-            $this->error("Cannot update project once it has been approved.", position: 'toast-bottom', redirectTo: route('projects.edit', $this->project->getRouteKey()));
+            $this->error("Cannot update project once it has been approved.", redirectTo: route('projects.edit', $this->project->getRouteKey()));
             return;
         }
 
@@ -55,12 +60,12 @@ class EditProject extends Component
     public function delete(Project $project): void
     {
         if (auth()->user()->hasRole(User::ROLE_STUDENT) && $this->project->status == Project::STATUS_APPROVED) {
-            $this->error("Cannot delete project once it has been approved.", position: 'toast-bottom', redirectTo: route('projects.edit', $this->project->getRouteKey()));
+            $this->error("Cannot delete project once it has been approved.", redirectTo: route('projects.edit', $this->project->getRouteKey()));
             return;
         }
 
         $project->delete();
-        $this->error("Deleting #$project->name", position: 'toast-bottom', redirectTo: route('projects.index'));
+        $this->error("Deleting #$project->name", redirectTo: route('projects.index'));
     }
 
     public function render()
