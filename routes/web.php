445f52d39409a71fec276b9fd1778cfc6befac2c
Marcus loo <Marcuslooek@gmail.com>
2025-06-16 16:33:08 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,6 @@ Route::get('/logout', function () {
 Route::middleware(['auth'])->group(function () {
     Route::prefix('student')->name('student.')->group(function () {
         Route::get('/dashboard', \App\Livewire\student\Dashboard::class)->name('dashboard');
-
-//        Route::get('/users', \App\Livewire\Users\UserList::class)->name('users.index');
-//        Route::get('/users/create', \App\Livewire\Users\CreateUser::class)->name('users.create');
-//        Route::get('/users/{user}/edit', \App\Livewire\Users\EditUser::class)->name('users.edit');
-
         Route::get('/projects', \App\Livewire\student\Projects\ProjectList::class)->name('projects.index');
         Route::get('/my-projects', \App\Livewire\student\Projects\MyProjects::class)->name('projects.self');
         Route::get('/my-project-request', \App\Livewire\student\Projects\ProjectRequest::class)->name('projects.requests');
@@ -32,4 +27,10 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/projects/{project}/edit', \App\Livewire\student\Projects\EditProject::class)->name('projects.edit');
         Route::get('/projects/{project}', \App\Livewire\student\Projects\ShowProject::class)->name('projects.show');
     })->middleware('student');
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('/users', \App\Livewire\admin\Users\UserList::class)->name('users.index');
+        Route::get('/users/create', \App\Livewire\admin\Users\CreateUser::class)->name('users.create');
+        Route::get('/users/{user}/edit', \App\Livewire\admin\Users\EditUser::class)->name('users.edit');
+    })->middleware('admin');
 });
